2cbf6edf6e30ddc2a4b06b83bedcd3b5cabd50a5
SEUNGYOON LEE <ssyjgs1@outlook.com>
2022-12-06 03:28:31 +0900

    delete unused package

--- 2022-12-06 삼국 철모 사이즈는 유사할까 + 각국마다 선호하는 특성이 있을까/quest.py
+++ 2022-12-06 삼국 철모 사이즈는 유사할까 + 각국마다 선호하는 특성이 있을까/quest.py
@@ -1,34 +1,9 @@
-from matplotlib.collections import PolyCollection
 from matplotlib import colors as mcolors
-from matplotlib.ticker import NullFormatter
-from matplotlib import transforms
-from scipy.integrate import simps
-from sklearn.impute import SimpleImputer
-from sklearn.datasets import make_regression
-from sklearn.linear_model import LinearRegression
-from calendar import isleap
-import datetime
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import matplotlib.tri as mtri
 import numpy as np
 import koreanize_matplotlib
-import string
-import scipy as sp
-import scipy.stats
-import random
-import missingno as msno
-import patsy
-import warnings
-warnings.filterwarnings('ignore')
-from patsy import demo_data
-from patsy import dmatrix
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.preprocessing import FunctionTransformer
 
 total_data = pd.read_csv("./total.csv")
 data_g = pd.read_csv("./고구려.csv")
@@ -55,6 +30,4 @@ for i, col1 in enumerate(col_names):
             plt.grid()
             siris = total_data[total_data['국가']==species]
             siris.plot.scatter(x=col1,y=col2, alpha=0.6,ax=ax[j][i], color=colors[k])
-            
-
 plt.show()
