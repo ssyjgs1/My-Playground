9aabbe22997e6bba978eade8f12afc44d0df57dc
SEUNGYOON LEE <ssyjgs1@outlook.com>
2023-06-30 00:05:17 +0900

    Update BizNo - Name - Addr1 - Addr2 - Phone - Seq - Weight.sql

--- Database Query/BizNo - Name - Addr1 - Addr2 - Phone - Seq - Weight.sql
+++ Database Query/BizNo - Name - Addr1 - Addr2 - Phone - Seq - Weight.sql
@@ -1,4 +1,4 @@
-SELECT FoodCorp.BizNo, FoodCorp.Name, FoodCorp.Addr1, FoodCorp.Addr2, Member.Phone, FoodWaste.Seq, FoodWaste.Weight, FoodCorp.RegistDate
+SELECT FoodCorp.BizNo, FoodCorp.Name, FoodCorp.Addr1, FoodCorp.Addr2, Member.Phone, FoodWaste.Seq, FoodWaste.Weight
 FROM
     (Member LEFT JOIN FoodCorp ON Member.MemberID = FoodCorp.MemberId
     LEFT JOIN FoodWaste ON Member.MemberID = FoodWaste.MemberID)
